9ffe8f7a15c82dc868d17e77d1f969f1d0bddb60
Uzbek150885 <Uzbek150885@gmail.com>
2021-09-17 21:19:37 +0300

    mA

---
+++ src/main/java/ru/netology/manager/Manager.java
@@ -0,0 +1,68 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Catalog;
+
+public class Manager {
+    private Catalog[] movies = new Catalog[0];
+    private int limit = 10;
+
+    public Manager() {
+    }
+
+    public Manager(int lim) {
+        if (lim < limit) {
+            this.limit = lim;
+        } else this.limit = 10;
+    }
+
+    public void add(Catalog movie) {
+        int length = movies.length + 1;
+        Catalog[] tmp = new Catalog[length];
+        System.arraycopy(movies, 0, tmp, 0, movies.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = movie;
+        movies = tmp;
+    }
+
+    public Catalog[] getAll() {
+        Catalog[] result = new Catalog[movies.length];
+            for (int i = 0; i < result.length; i++) {
+            int index = movies.length - i - 1;
+            result[i] = movies[index];
+        }
+        return result;
+    }
+
+    public void removeById(int id) {
+        int length = movies.length - 1;
+        Catalog[] tmp = new Catalog[length];
+        int index = 0;
+        for (Catalog movie : movies) {
+            if (movie.getId() != id) {
+                tmp[index] = movie;
+                index++;
+            }
+        }
+        movies = tmp;
+    }
+
+    public Catalog[] getLast() {
+
+        int resultLength = movies.length + 1;
+        if (movies.length > limit) {
+            resultLength = limit;
+        } else {
+            resultLength = movies.length;
+        }
+        Catalog[] result = new Catalog[resultLength];
+
+        for (int i = 0; i < result.length; i++) {
+            int index = result.length - i - 1;
+            result[i] = movies[index];
+        }
+        return result;
+    }
+
+}
+
+
---
+++ src/test/java/ru/netology/manager/ManagerTest.java
@@ -0,0 +1,127 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Catalog;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class ManagerTest {
+    @Test
+    public void shouldRemoveIfExists() {
+        Manager manager = new Manager();
+        int idToRemove = 1;
+        Catalog first = new Catalog(1, 1, "Бладшот", 1, 1);
+        Catalog second = new Catalog(2, 2, "Вперед", 1, 1);
+        Catalog third = new Catalog(3, 3, "Отель Белград", 1, 1);
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+
+        manager.removeById(idToRemove);
+
+        Catalog[] actual = manager.getAll();
+        Catalog[] expected = new Catalog[]{third, second};
+
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void ShouldShowCatalogMovieAboveLim() {
+        Manager manager = new Manager();
+
+        Catalog first = new Catalog(1, 1, "Бладшот", 1, 1);
+        Catalog second = new Catalog(2, 2, "Вперед", 1, 1);
+        Catalog third = new Catalog(3, 3, "Отель Белград", 1, 1);
+        Catalog four = new Catalog(4, 4, "Джентельмены", 1, 1);
+        Catalog five = new Catalog(5, 5, "Человек невидимка", 1, 1);
+        Catalog six = new Catalog(6, 6, "Тролли", 1, 1);
+        Catalog seven = new Catalog(7, 7, "Номер один", 1, 1);
+        Catalog eight = new Catalog(8, 8, "Поезд в Пусан", 1, 1);
+        Catalog nine = new Catalog(9, 9, "Револьвер", 1, 1);
+        Catalog ten = new Catalog(10, 10, "Гнев человеческий", 1, 1);
+        Catalog eleven = new Catalog(11, 11, "Чернобыль", 1, 1);
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(four);
+        manager.add(five);
+        manager.add(six);
+        manager.add(seven);
+        manager.add(eight);
+        manager.add(nine);
+        manager.add(ten);
+        manager.add(eleven);
+
+        manager.getLast();
+
+        Catalog[] actual = manager.getLast();
+        Catalog[] expected = new Catalog[]{ten, nine, eight, seven, six, five, four,
+                third, second, first};
+
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void ShouldShowCatalogMovieBelowMLim() {
+        Manager manager = new Manager();
+
+        Catalog first = new Catalog(1, 1, "Бладшот", 1, 1);
+        Catalog second = new Catalog(2, 2, "Вперед", 1, 1);
+        Catalog third = new Catalog(3, 3, "Отель Белград", 1, 1);
+        Catalog four = new Catalog(4, 4, "Джентельмены", 1, 1);
+        Catalog five = new Catalog(5, 5, "Человек невидимка", 1, 1);
+        Catalog six = new Catalog(6, 6, "Тролли", 1, 1);
+        Catalog seven = new Catalog(7, 7, "Номер один", 1, 1);
+        Catalog eight = new Catalog(8, 8, "Поезд в Пусан", 1, 1);
+        Catalog nine = new Catalog(9, 9, "Револьвер", 1, 1);
+        Catalog ten = new Catalog(10, 10, "Гнев человеческий", 1, 1);
+        Catalog eleven = new Catalog(11, 11, "Чернобыль", 1, 1);
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+
+        manager.getLast();
+
+        Catalog[] actual = manager.getLast();
+        Catalog[] expected = new Catalog[]{third, second, first};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldShowLimCatalogMovie() {
+        Manager manager = new Manager();
+
+        Catalog first = new Catalog(1, 1, "Бладшот", 1, 1);
+        Catalog second = new Catalog(2, 2, "Вперед", 1, 1);
+        Catalog third = new Catalog(3, 3, "Отель Белград", 1, 1);
+        Catalog four = new Catalog(4, 4, "Джентельмены", 1, 1);
+        Catalog five = new Catalog(5, 5, "Человек невидимка", 1, 1);
+        Catalog six = new Catalog(6, 6, "Тролли", 1, 1);
+        Catalog seven = new Catalog(7, 7, "Номер один", 1, 1);
+        Catalog eight = new Catalog(8, 8, "Поезд в Пусан", 1, 1);
+        Catalog nine = new Catalog(9, 9, "Револьвер", 1, 1);
+        Catalog ten = new Catalog(10, 10, "Гнев человеческий", 1, 1);
+        Catalog eleven = new Catalog(11, 11, "Чернобыль", 1, 1);
+
+
+        manager.add(second);
+        manager.add(third);
+        manager.add(four);
+        manager.add(five);
+        manager.add(six);
+        manager.add(seven);
+        manager.add(eight);
+        manager.add(nine);
+        manager.add(ten);
+        manager.add(eleven);
+
+        manager.getLast();
+
+        Catalog[] actual = manager.getLast();
+        Catalog[] expected = new Catalog[]{eleven, ten, nine, eight, seven, six, five, four,
+                third, second};
+               assertArrayEquals(expected, actual);
+    }
+
+}
